1dd4df5418fa1c851ca4bf69320c4b0b504b302f
yacineatmani <yacineatmani1080@gmail.com>
2025-03-24 05:49:20 +0100

    Correction de layout.tsx pour supprimer l'erreur d'hydratation

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/Next-Libraire',
-  assetPrefix: '/Next-Libraire/',
   images: {
-    domains: ['images.gr-assets.com'], // Conserver votre domaine
-    unoptimized: true, // Nécessaire pour l'export statique
+    domains: ['images.gr-assets.com'],
+    unoptimized: true, // Si tu n'avais pas ce paramètre avant, retire-le
   },
-  // Autres options de configuration si nécessaire
 };
 
-// Utiliser module.exports au lieu de export default pour la compatibilité
 module.exports = nextConfig;
