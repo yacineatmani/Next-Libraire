702e77486bebeb4a6404cbc1217b6442fa590f5f
yacineatmani <yacineatmani1080@gmail.com>
2025-05-15 14:26:41 +0200

    github page 3

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   images: {
     domains: ['images.gr-assets.com'],
-    unoptimized: true, // Si tu n'avais pas ce paramètre avant, retire-le
+    unoptimized: true,
   },
-};
-// next.config.js
-const isGithubPages = process.env.GITHUB_ACTIONS || false;
-
-module.exports = {
   output: 'export',
   basePath: isGithubPages ? '/Next-Libraire' : '',
   trailingSlash: true,
 };
 
-
-module.exports = nextConfig;
+export default nextConfig;
