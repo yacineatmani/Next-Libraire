beba91377d33146533f60dc0a05a2c1693a03877
yacineatmani <yacineatmani1080@gmail.com>
2025-05-15 14:17:19 +0200

    github page

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,14 @@ const nextConfig = {
     unoptimized: true, // Si tu n'avais pas ce paramètre avant, retire-le
   },
 };
+// next.config.js
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
+
+module.exports = {
+  output: 'export',
+  basePath: isGithubPages ? '/Next-Libraire' : '',
+  trailingSlash: true,
+};
+
 
 module.exports = nextConfig;
